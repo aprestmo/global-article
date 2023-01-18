ba1626415273ad26f0d88037dee3dfcf383eb051
Alexander Prestmo <aprestmo@gmail.com>
2023-01-18 16:02:32 +0100

    Dummy article with color theme options

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import vue from "@astrojs/vue";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), vue()]
+});
