d88007627a0c7d29931d53afb14c0906b4159bed
Alexander Prestmo <aprestmo@gmail.com>
2023-04-24 13:41:00 +0200

    Add all publications

---
+++ src/data/navData.js
@@ -0,0 +1,52 @@
+const navData = [
+  {
+    publication: 'DN',
+    path: '/dn/',
+  },
+  {
+    publication: 'D2',
+    path: '/d2/',
+  },
+  {
+    publication: 'Fiskeribladet',
+    path: '/fiskeribladet/',
+  },
+  {
+    publication: 'TradeWinds',
+    path: '/tradewinds/',
+  },
+  {
+    publication: 'Upstream',
+    path: '/upstream/',
+  },
+  {
+    publication: 'IntraFish (no)',
+    path: '/intrafish-no/',
+  },
+  {
+    publication: 'IntraFish (eng)',
+    path: '/intrafish-eng/',
+  },
+  {
+    publication: 'Europower',
+    path: '/europower/',
+  },
+  {
+    publication: 'Recharge',
+    path: '/recharge/',
+  },
+  {
+    publication: 'Hydrogen Insight',
+    path: '/hydrogen-insight/',
+  },
+  {
+    publication: 'Kystens Næringsliv',
+    path: '/kystens/',
+  },
+  {
+    publication: 'White Label',
+    path: '/white-label/',
+  },
+]
+
+export default navData;
