c0b5b40a40c31c2082da148ce3e96445f58939ae
Marius Grøndahl <hei@mariusgrondahl.tech>
2023-05-11 16:19:30 +0200

    added colors

--- src/data/navData.js
+++ src/data/navData.js
@@ -1,56 +1,72 @@
 const navData = [
   {
-    publication: 'White Label',
-    path: '/white-label/',
+    publication: "White Label",
+    path: "/white-label/",
   },
   {
-    publication: 'Recharge',
-    path: '/recharge/',
+    publication: "Recharge",
+    path: "/recharge/",
+    background:
+      "https://www.dngroup.com/wp-content/uploads/2023/02/recharge-cover-855x1024.png",
   },
   {
-    publication: 'DN',
-    path: '/dn/',
+    publication: "DN",
+    path: "/dn/",
+    background:
+      "https://www.dngroup.com/wp-content/uploads/2023/02/DN-cover-855x1024.png",
   },
-  // {
-  //   publication: 'Europower',
-  //   path: '/europower/',
-  // },
-  // {
-  //   publication: 'Fiskeribladet',
-  //   path: '/fiskeribladet/',
-  // },
-  // {
-  //   publication: 'TradeWinds',
-  //   path: '/tradewinds/',
-  // },
-  // {
-  //   publication: 'Upstream',
-  //   path: '/upstream/',
-  // },
-  // {
-  //   publication: 'IntraFish (no)',
-  //   path: '/intrafish-no/',
-  // },
-  // {
-  //   publication: 'IntraFish (eng)',
-  //   path: '/intrafish-eng/',
-  // },
-  // {
-  //   publication: 'Recharge',
-  //   path: '/recharge/',
-  // },
-  // {
-  //   publication: 'Hydrogen Insight',
-  //   path: '/hydrogen-insight/',
-  // },
-  // {
-  //   publication: 'Kystens Næringsliv',
-  //   path: '/kystens/',
-  // },
-  // {
-  //   publication: 'D2',
-  //   path: '/d2/',
-  // },
-]
+  {
+    publication: "Europower",
+    path: "/europower/",
+    background:
+      "https://www.dngroup.com/wp-content/uploads/2023/02/europower-cover-855x1024.png",
+  },
+  {
+    publication: "Fiskeribladet",
+    path: "/fiskeribladet/",
+    background:
+      "https://www.dngroup.com/wp-content/uploads/2023/02/fiskerbladet-cover-855x1024.png",
+  },
+  {
+    publication: "TradeWinds",
+    path: "/tradewinds/",
+    background:
+      "https://www.dngroup.com/wp-content/uploads/2023/02/TradeWinds-cover-855x1024.png",
+  },
+  {
+    publication: "Upstream",
+    path: "/upstream/",
+    background:
+      "https://www.dngroup.com/wp-content/uploads/2023/02/upstream-cover-855x1024.png",
+  },
+  {
+    publication: "IntraFish (no)",
+    path: "/intrafish-no/",
+    background:
+      "https://www.dngroup.com/wp-content/uploads/2023/02/intrafish-cover-855x1024.png",
+  },
+  {
+    publication: "IntraFish (eng)",
+    path: "/intrafish-eng/",
+    background:
+      "https://www.dngroup.com/wp-content/uploads/2023/02/intrafish-cover-.com_-855x1024.png",
+  },
+  {
+    publication: "Hydrogen Insight",
+    path: "/hydrogen-insight/",
+    background:
+      "https://www.dngroup.com/wp-content/uploads/2023/02/Hydrogeninsight-cover-855x1024.png",
+  },
+  {
+    publication: "Kystens Næringsliv",
+    path: "/kystens/",
+    background:
+      "https://www.dngroup.com/wp-content/uploads/2023/02/Kystens-cover-855x1024.png",
+  },
+  {
+    publication: "D2",
+    path: "/d2/",
+  },
+];
 
 export default navData;
