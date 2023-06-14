9a963e9b4eb731f7dfe97cce8cb743a719a9173a
Marius Grøndahl <hei@mariusgrondahl.tech>
2023-06-14 10:13:13 +0200

    fixes

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -225,6 +225,15 @@ declare module 'astro:content' {
   data: any
 } & { render(): Render[".mdx"] };
 };
+"fiskeribladet": {
+"fiskeribladet.mdx": {
+	id: "fiskeribladet.mdx";
+  slug: "fiskeribladet";
+  body: string;
+  collection: "fiskeribladet";
+  data: any
+} & { render(): Render[".mdx"] };
+};
 "hydrogen": {
 "hydrogen.mdx": {
 	id: "hydrogen.mdx";
@@ -240,6 +249,24 @@ declare module 'astro:content' {
   slug: "intrafish";
   body: string;
   collection: "intrafish";
+  data: any
+} & { render(): Render[".mdx"] };
+};
+"kystens": {
+"kystens.mdx": {
+	id: "kystens.mdx";
+  slug: "kystens";
+  body: string;
+  collection: "kystens";
+  data: any
+} & { render(): Render[".mdx"] };
+};
+"recharge": {
+"recharge.mdx": {
+	id: "recharge.mdx";
+  slug: "recharge";
+  body: string;
+  collection: "recharge";
   data: any
 } & { render(): Render[".mdx"] };
 };
@@ -265,13 +292,7 @@ declare module 'astro:content' {
 	};
 
 	type DataEntryMap = {
-		"fiskeribladet": {
-};
-"kystens": {
-};
-"recharge": {
-};
-
+		
 	};
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
